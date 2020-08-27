635f063e28ac695dd8159c9c65aab62e7b8c0f14
lv xun <lvxun@lvtingxun.local>
2020-08-27 16:03:20 +0800

    add video classifier

--- ts/torch_handler/video_classifier.py
+++ ts/torch_handler/video_classifier.py
@@ -15,16 +15,35 @@ class VideoClassifier(VideoHandler):
     and returns the name of object in that image.
     """
 
-    topk = 5
+    topk = 1
     # These are the standard Imagenet dimensions
     # and statistics
-    image_processing = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225])
-    ])
+    softmax = F.Softmax(dim=-1)
+
+    normalize = transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            normalize
+        ])
+
+
+    def preprocess(self, data):
+        """
+        x: NFCHW: tensor CPU; C = 3: RGB order
+        return: list of str ['Game', 'Cartoo', 'Other (Entertainment)'] (len N)
+        """
+        x = super().preprocess(data)
+        bs, n_frame = x.size(0), x.size(1)
+        x = torch.squeeze(x[:, n_frame // 2, ...])
+        for i in range(bs):
+            x_ = transform(x[i, ...])[None, ...]
+            return x_
+
+
 
     def set_max_result_classes(self, topk):
         self.topk = topk
@@ -33,7 +52,7 @@ class VideoClassifier(VideoHandler):
         return self.topk
 
     def postprocess(self, data):
-        ps = F.softmax(data, dim=1)
+        ps = softmax(game_pred).data.cpu().numpy()[:, 1]
         probs, classes = torch.topk(ps, self.topk, dim=1)
         probs = probs.tolist()
         classes = classes.tolist()
